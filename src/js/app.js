9ad3ae1076fbff9e25494396d5544969e0d554e9
Ruslan Arshynnyk <ruslanarshynnyk@gmail.com>
2021-05-11 12:49:59 +0300

    feat: initial commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+// You can write a call and import your functions in this file.
+//
+// This file will be compiled into app.js
+// Feel free with using ES6 here.
+import $ from 'jquery';
+
+import dots from './modules/dots';
+
+// When DOM is ready
+$(() => {
+  dots();
+});
---
+++ src/js/modules/dots.js
@@ -0,0 +1,28 @@
+// This is an examples of simple export.
+//
+// You can remove or add your own function in this file.
+import $ from 'jquery';
+
+const _createCells = () => {
+  const width = 10;
+  const height = 10;
+  const size = width * height;
+  let html = '<div class="entry-cells">';
+
+  for (let i = 0; i < size; i += 1) {
+    html += `<div class="cell cell-${i}"></div>`;
+  }
+
+  html += '</div>';
+
+  return html;
+};
+
+const dots = () => {
+  const $cnt = $('.entry-section');
+  const cells = _createCells();
+
+  $cnt.html(cells);
+};
+
+export default dots;
